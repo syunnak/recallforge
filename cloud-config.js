aa8720fdac5d5c743c9195ef17ca72a06ca88b39
syunnak <syunnak1301@gmail.com>
2026-07-03 19:31:01 +0900

    Add Supabase configuration to cloud-config.js

---
+++ cloud-config.js
@@ -0,0 +1,5 @@
+window.RECALLFORGE_SUPABASE = {
+  url: "https://akacimuwntjohnydymbv.supabase.co",
+  anonKey: "sb_publishable_Fd6YAwO6WnOt6uJ4_3nOYQ_4Q8BU1LD",
+  defaultEmail: "syunnak1301@gmail.com"
+};
